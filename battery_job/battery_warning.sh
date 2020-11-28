cee443af81c2387051fe34eea456e32df0282fd8
Saketh Katari <katari15045@iiitd.ac.in>
2020-11-28 16:58:16 +0530

    battery-job: added upper-bound warning

--- battery_job/battery_warning.sh
+++ battery_job/battery_warning.sh
@@ -2,20 +2,31 @@
 
 CAPACITY_FILE="/sys/class/power_supply/BAT0/capacity"
 STATUS_FILE="/sys/class/power_supply/BAT0/status"
-CRITICAL_LEVEL=40
+CAPACITY_LOWER_BOUND=40
+CAPACITY_UPPER_BOUND=90
 
 CUR_CAPACITY=$(cat ${CAPACITY_FILE})
+CUR_STATUS=$(cat ${STATUS_FILE})
 
 
-if [ ${CUR_CAPACITY} -le ${CRITICAL_LEVEL} ]
+notify(){
+	export DISPLAY=:0
+	export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus
+	notify-send "$1"
+}
+
+if [ ${CUR_CAPACITY} -le ${CAPACITY_LOWER_BOUND} ]
 then
-	CUR_STATUS=$(cat ${STATUS_FILE})
-
 	if [ "${CUR_STATUS}" == "Discharging" ]
 	then
-		export DISPLAY=:0
-		export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${UID}/bus
-		notify-send "Battery Low, ${CUR_CAPACITY}%. Plug-in Charger Immediately"
-f
+		notify "Battery Low, ${CUR_CAPACITY}%. Plug-in Charger Immediately"
+	fi
+elif [ ${CUR_CAPACITY} -ge ${CAPACITY_UPPER_BOUND} ]
+then
+	if [ "${CUR_STATUS}" == "Charging" ]
+	then
+                notify "Battery almost full, ${CUR_CAPACITY}%. Unplug charger"
 	fi
 fi
+
+
